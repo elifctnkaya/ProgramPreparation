c7d8c030db651f8393b5b767be5fef0afe344aba
elifcetinkaya <elfctnkaya1@gmail.com>
2020-06-22 23:34:34 +0300

    first commit

---
+++ app/src/main/java/com/example/takvimdeneme/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.takvimdeneme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        textView = (TextView)findViewById(R.id.textView);
+        editText.getText().toString();
+    }
+}
