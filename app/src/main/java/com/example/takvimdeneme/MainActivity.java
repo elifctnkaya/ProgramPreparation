7f475ff35f3bbb355638e923cdad255ed0a67c1c
dogus <dogusdemirkiran@gmail.com>
2020-06-23 20:52:17 +0300

    Yeni Activity eklendi

--- app/src/main/java/com/example/takvimdeneme/MainActivity.java
+++ app/src/main/java/com/example/takvimdeneme/MainActivity.java
@@ -2,25 +2,31 @@ package com.example.takvimdeneme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    EditText editText;
-    Button button1, button2;
+    private TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText)findViewById(R.id.editText);
-        textView = (TextView)findViewById(R.id.textView);
-        editText.getText().toString();
-        //deneme
+        textView1 = findViewById(R.id.editText);
+        duzenle(textView1);
     }
+
+    public void duzenle(View v){
+        Toast.makeText(getApplicationContext(),"True",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this,EditActivity.class);
+        startActivity(intent);
+    }
+
 }
