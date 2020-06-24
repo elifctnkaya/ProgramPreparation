554f679dc2a7fb4c6d6848f751f949fd2eebf86e
dogus <dogusdemirkiran@gmail.com>
2020-06-24 22:15:20 +0300

    Yeni Activity eklendi

--- app/src/main/java/com/example/takvimdeneme/EditActivity.java
+++ app/src/main/java/com/example/takvimdeneme/EditActivity.java
@@ -3,12 +3,33 @@ package com.example.takvimdeneme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText1;
+    private EditText editText2;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        textView = findViewById(R.id.baslik);
+        editText1 = findViewById(R.id.dersAdi);
+        editText2 = findViewById(R.id.hocaismi);
+        button = findViewById(R.id.ekle);
+        db_ekle(button);
     }
+
+    public void db_ekle(View view){
+        Toast.makeText(getApplicationContext(),"Eklendi",Toast.LENGTH_SHORT).show();
+    }
+
 }
