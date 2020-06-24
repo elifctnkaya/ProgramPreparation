67e4bd027a9116ab097507f7497619bb38e11988
elifcetinkaya <elfctnkaya1@gmail.com>
2020-06-24 21:55:42 +0300

    fragment

--- app/src/main/java/com/example/takvimdeneme/MainActivity.java
+++ app/src/main/java/com/example/takvimdeneme/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.takvimdeneme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,14 +14,33 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button2;
     private TextView textView1;
+
+    public void gecis(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame,fragment);
+        fragmentTransaction.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView1 = findViewById(R.id.editText);
+        gecis(new Fragment1());
+
+        button2 = findViewById(R.id.buton2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gecis(new Fragment2());
+
+            }
+        });
+
+        //textView1 = findViewById(R.id.editText);
         duzenle(textView1);
     }
 
